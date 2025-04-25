e468ead14216102e1949945adecd92a1bb747766
Xiaotian Wu <wuxiaotian@loongson.cn>
2025-04-25 17:32:12 +0800

    add libgtk-3.0

--- update.sh
+++ update.sh
@@ -9,13 +9,14 @@
 ######################################################################
 
 sources=(
-  "https://pkg.loongnix.cn/loongnix/pool/main/t/tiff/libtiff5_4.1.0+git191117-2~deb10u1.lnd.4_loongarch64.deb"
-  "https://pkg.loongnix.cn/loongnix/pool/main/libw/libwebp/libwebp6_0.6.1-2.lnd.2_loongarch64.deb"
+  "https://pkg.loongnix.cn/loongnix/pool/main/g/gtk+3.0/libgtk-3-0_3.24.5-1.lnd.1_loongarch64.deb"
   "https://pkg.loongnix.cn/loongnix/pool/main/j/jbigkit/libjbig0_2.1-3.2_loongarch64.deb"
   "https://pkg.loongnix.cn/loongnix/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.2-2.lnd.4_loongarch64.deb"
+  "https://pkg.loongnix.cn/loongnix/pool/main/libw/libwebp/libwebp6_0.6.1-2.lnd.2_loongarch64.deb"
   "https://pkg.loongnix.cn/loongnix/pool/main/p/pango1.0/libpango-1.0-0_1.42.4-8~deb10u1.lnd.1_loongarch64.deb"
-  "https://pkg.loongnix.cn/loongnix/pool/main/p/pango1.0/libpangoft2-1.0-0_1.42.4-8~deb10u1.lnd.1_loongarch64.deb"
   "https://pkg.loongnix.cn/loongnix/pool/main/p/pango1.0/libpangocairo-1.0-0_1.42.4-8~deb10u1.lnd.1_loongarch64.deb"
+  "https://pkg.loongnix.cn/loongnix/pool/main/p/pango1.0/libpangoft2-1.0-0_1.42.4-8~deb10u1.lnd.1_loongarch64.deb"
+  "https://pkg.loongnix.cn/loongnix/pool/main/t/tiff/libtiff5_4.1.0+git191117-2~deb10u1.lnd.4_loongarch64.deb"
 )
 
 for i in ${sources[@]};
